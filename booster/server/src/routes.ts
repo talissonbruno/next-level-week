96ece5d815f933960806bc9355f2b30b2dc3c285
Talisson Souza <talissonbrsouza@gmail.com>
2020-06-03 11:09:06 -0300

    server parte backend #005

--- booster/server/src/routes.ts
+++ booster/server/src/routes.ts
@@ -6,7 +6,14 @@ const routes = express.Router();
 routes.get('/items', async (request, response)=>{
     const items = await knex('items').select('*');
 
-    return response.json({ message: 'Hello world' });
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        }
+    });
+
+    return response.json(serializedItems);
 });
 
 export default routes;
