b2b35b1644ae7b01d878ba392b1f8fd66fec6217
Talisson Souza <talissonbrsouza@gmail.com>
2020-06-02 22:52:58 -0300

    server parte backend #001

--- booster/server/src/server.ts
+++ booster/server/src/server.ts
@@ -2,19 +2,42 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = [
+    'Talisson',
+    'Brunno',
+    'Dhimerson',
+    'joao',
+    'Camila'
+];
+
 app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
+    const search = String(request.query.search);
 
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
 
-
-    response.json([
-        'Talisson',
-        'Brunno',
-        'Dhimerson',
-        'joao',
-        'Camila'
-    ]);
+    response.json(filteredUsers);
 });
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (resquest, response) =>{
+
+    const data =resquest.body;
+
+    const user = { 
+       name: data.name,
+       email: data.email
+    };
+    return response.json(user);
+})
 
 app.listen(3333);
 
